069ba908d2aaf3ec332c5127ceaa55c04aeec08d
burghausen8 <burghausen8@gmail.com>
2024-05-23 18:33:06 -0300

    add category item

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/category_item.dart';
+import '../data/dummy_data.dart';
 
-class Categories extends StatelessWidget {
-  const Categories({super.key});
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos cozinhar?'),
       ),
-      children: [],
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
